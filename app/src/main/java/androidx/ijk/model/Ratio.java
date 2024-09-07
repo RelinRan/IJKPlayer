5568ab3f8252e0b5fca1f8531f2dc4c077b138fa
relinran <1101005304@qq.com>
2024-09-07 14:17:00 +0800

    1.VideoView新增setRatio方法设置显示比例 2.手势滑动显示多指滑动

---
+++ app/src/main/java/androidx/ijk/model/Ratio.java
@@ -0,0 +1,48 @@
+package androidx.ijk.model;
+
+/**
+ * 视频宽高比例
+ */
+public class VideoRatio {
+
+    /**
+     * 宽度比例值
+     */
+    private int width;
+    /**
+     * 高度比例值
+     */
+    private int height;
+
+    /**
+     * 构建视频宽高比例, 例如 4:3 16:9
+     *
+     * @param width  例如16
+     * @param height 例如9
+     */
+    public VideoRatio(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public float getRatio(){
+        return width*1.0f/height;
+    }
+
+}
